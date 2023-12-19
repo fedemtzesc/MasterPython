78d8a713ccdc638ff2bb441acaf6f9d9a0b8b6d8
Federico Martinez E <28632297+fedemtzesc@users.noreply.github.com>
2023-12-19 01:09:01 -0600

    Primeros archivos del curso

---
+++ capitulos/Cap-8/comentarios.py
@@ -0,0 +1,14 @@
+# Asi se escribe un comentario en python
+print("Hola")
+
+# Estas lineas muestran texto en pantalla
+print("Mundo")
+print("!!")
+
+
+'''
+    Asi se escriben
+    los comentarios
+    que ocupan
+    muchas lineas
+'''
